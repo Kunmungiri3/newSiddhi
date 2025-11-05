4dfc4f1dd3861c8d79e1a0adb9d0ca76690ed18d
Kunmungiri3 <kunmungiri1@gmail.com>
2025-11-05 22:50:54 +0530

    hr

--- routes/jobseeker.js
+++ routes/jobseeker.js
@@ -12,7 +12,11 @@ const storage = multer.memoryStorage();
 const upload = multer({
   storage,
   limits: { fileSize: 2 * 1024 * 1024 }, // 2 MB max
-});
+  fileFilter: (req,file, cb) =>{
+    if (file.mimetype==="application/pdf") cb(null,true);
+    else cb(new Error('Only PDF files are allowed!'), false);
+  }}, 
+);
 
 // ================= Routes =================
 
